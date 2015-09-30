5438fb5e7ddb7a84d3746376bf47d2d473b50eb1
1shark1 <lukas.mateju1@gmail.com>
2015-09-30 14:15:15 +0200

    new version of settings, commented, new functionality added

--- settings.lua
+++ settings.lua
@@ -2,67 +2,70 @@
 require 'torch'
 require 'logroll'
 
+-- LM
+
+-- Script settings
+
 function Settings()
     
   local settings = {
     
-    --CZ
-    inputSize = 39
-    outputSize = 3886;
-    
-    --PL
-		--outputSize = 3180;
-    
-    --SK
-    --outputSize = 2377;
-    
-    --HR
-    --outputSize = 2183;
-    
-    --RU
-    --outputSize = 2593;
-    
-    seqL = 5;
-    seqR = 5;
-    
-    noNeurons = 512;
-    noHiddenLayers = 5;
+    -- DNN settings
+    inputSize = 39;           -- number of input features
+    outputSize = 3886;        -- number of output neurons (= align states)
+		--outputSize = 3180;      -- PL
+    --outputSize = 2377;      -- SK
+    --outputSize = 2183;      -- HR
+    --outputSize = 2593;      -- RU 
+    noHiddenLayers = 5;     
+    noNeurons = torch.Tensor({1024, 1024, 768, 768, 512, 512});       -- size: noHiddenLayers + 1
     noEpochs = 150;
-    learningRate = 0.08;
-    learningRateDecay = 0.1;
     batchSize = 1024;
+    learningRate = 0.08;
+    lRDecayActive = 0;        -- 1 active
+    learningRateDecay = 0.1;
+    dropout = 0;              -- 1 active
+    dropoutThreshold = torch.Tensor({0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1});  -- size: number of layers
+    seqL = 5;                 -- number of frames added as input - left
+    seqR = 5;                 -- number of frames added as input - right
     startEpoch = 0;
-    cms = 100;
-    cmsActive = 0;
-    dropout = 1;
-    dropoutThreshold = 0.1;
+    borders = 0;              -- 1 active   
+    cmsActive = 0;            -- 1 active
+    cms = 100;                -- window for calculation of CMS, only for stats&datasetFLMS
     
+    -- cuda on/off
+    cuda = 1;                 -- 1 active
+    
+    -- input lists    
     trainFile = 'cz-train-new.list';
     validFile = 'cz-valid-new.list';
     testFile = 'cz-test-new.list';
     
-    outputFolder = "/data/nnModels/2015-05-29";
-       
+    -- input folders
     inputPath = "/wav/";
-    mfccPath = "/fbank52/";
+    mfccPath = "/fbank39/";
     akuPath = "/akulab/";
+    -- input extension
     mfccExt = ".par";
     akuExt = ".akulab";
     
+    -- output folders
+    outputFolder = "/data/nnModels/2015-09-29";
     statsFolder = "/stats";
     logFolder = "/log";
     modFolder = "/mod";
-    
-    borders = 0;
 
   }  
   
-  settings.mean = torch.Tensor(settings.inputSize):zero();
-  settings.var = torch.Tensor(settings.inputSize):fill(1);
+  -- default global stats
+  settings.mean = torch.Tensor(settings.inputSize):zero();    
+  settings.var = torch.Tensor(settings.inputSize):fill(1);    
   
+  -- log
   flog = logroll.file_logger(settings.outputFolder .. settings.logFolder .. '/settings.log');
   flog.info(settings);
   
+  -- create output folders
   os.execute("mkdir " .. settings.outputFolder);
   os.execute("mkdir " .. settings.outputFolder .. settings.statsFolder);
   os.execute("mkdir " .. settings.outputFolder .. settings.logFolder);
