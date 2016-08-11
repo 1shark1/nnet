9fe1f0034b6a2ff81ffa6224a6e80d3cc2b6fa94
1shark1 <lukas.mateju1@gmail.com>
2016-08-11 12:42:36 +0200

    support for save/load packages

--- dataset.lua
+++ dataset.lua
@@ -1,9 +1,9 @@
 
--- LM -- Dataset -- 3/8/16 --
+-- LM -- Dataset -- 11/8/16 --
 
 
 
-function Dataset(list, isFilelist, computeFramestats, decode)
+function Dataset(list, isFilelist, computeFramestats, loadPackageData, savePackageData, decode)
   
   -- initialization
   local dataset = {}
@@ -33,108 +33,147 @@ function Dataset(list, isFilelist, computeFramestats, decode)
   
   -- initialize sample count
   local totalSamples = 0
+  local globalSamples = 0
   
-  -- check if input is file or filelist and get file name(s)
-  local filelist = {}
-  if isFilelist then
-    filelist = readFilelist(list)
-  else
-    table.insert(filelist, list)
-  end
+  -- process data / load saved package
+  if not loadPackageData then
   
-  -- initialize framestats
-  if not decode then
-    for i = 1, settings.outputSize, 1 do
-      dataset.framestats[i] = 0
-    end
-  end
-  
-  -- read data from files
-  for file = 1, #filelist, 1 do  
-    
-    -- log
-    if not decode then
-      flog.info('Processing file: ' .. filelist[file])
-    end
-    
-    local nSamples, sampPeriod, sampSize, parmKind, fvec, viewRefs
-    
-    -- read input files
-    if settings.inputView > 0 then
-      nSamples, sampPeriod, sampSize, parmKind, fvec, viewRefs = readView(filelist[file], true)
+    -- check if input is file or filelist and get file name(s)
+    local filelist = {}
+    if isFilelist then
+      filelist = readFilelist(list)
     else
-      nSamples, sampPeriod, sampSize, parmKind, fvec = readInputs(filelist[file])
+      table.insert(filelist, list)
     end
     
-    -- read refs files
-    local references
-    if not decode then 
-      if settings.sameFolder == 0 then
-        filelist[file] = filelist[file]:gsub(settings.parPath, settings.refPath)
+    -- initialize framestats
+    if not decode then
+      for i = 1, settings.outputSize, 1 do
+        dataset.framestats[i] = 0
       end
-
-      if settings.inputView > 0 then
-        references = viewRefs
-      else
-        references = readRefs(filelist[file], nSamples)
-      end    
+    end
+    
+    -- read data from files
+    for file = 1, #filelist, 1 do  
       
-      -- compute framestats
-      if computeFramestats then
-        for i = 1, references:size(1), 1 do
-          if references[i] < settings.outputSize then
-            dataset.framestats[references[i]+1] = dataset.framestats[references[i]+1] + 1
+      -- log
+      if not decode then
+        flog.info('Processing file: ' .. filelist[file])
+      end
+      
+      local nSamples, sampPeriod, sampSize, parmKind, fvec, viewRefs
+      
+      -- read input files
+      if settings.inputView > 0 then
+        nSamples, sampPeriod, sampSize, parmKind, fvec, viewRefs = readView(filelist[file], true)
+      else
+        nSamples, sampPeriod, sampSize, parmKind, fvec = readInputs(filelist[file] .. settings.parExt)
+      end
+      
+      -- read refs files
+      local references
+      if not decode then 
+        if settings.sameFolder == 0 then
+          filelist[file] = filelist[file]:gsub(settings.parPath, settings.refPath)
+        end
+
+        if settings.inputView > 0 then
+          references = viewRefs
+        else
+          references = readRefs(filelist[file] .. settings.refExt, nSamples)
+        end    
+        
+        -- compute framestats
+        if computeFramestats then
+          for i = 1, references:size(1), 1 do
+            if references[i] < settings.outputSize then
+              dataset.framestats[references[i]+1] = dataset.framestats[references[i]+1] + 1
+            end
           end
         end
       end
-    end
 
-    -- clone borders
-    if settings.cloneBorders == 1 then
-      fvec = cloneBordersInputs(fvec)
-      if not decode then 
-        references = cloneBordersRefs(references)
+      -- clone borders
+      if settings.cloneBorders == 1 then
+        fvec = cloneBordersInputs(fvec)
+        if not decode then 
+          references = cloneBordersRefs(references)
+        end
+        nSamples = nSamples + settings.seqL + settings.seqR
       end
-      nSamples = nSamples + settings.seqL + settings.seqR
-    end
+        
+      -- apply CMS
+      if settings.applyCMS == 1 then
+        applyCMS(fvec, nSamples)
+      end    
       
-    -- apply CMS
-    if settings.applyCMS == 1 then
-      applyCMS(fvec, nSamples)
-    end    
-    
-    -- store data to memory
-    fvec = fvec:view(fvec:size(1) * settings.inputSize)
-    table.insert(dataset.cache, {inp = fvec, out = references})
-    
-    -- save counts of samples per file to table
-    nSamples = nSamples - settings.seqL - settings.seqR
-    table.insert(dataset.nSamplesList, nSamples)
-    
-    -- calculate total samples
-    if nSamples >= 1 then
-      totalSamples = totalSamples + nSamples
+      -- store data to memory
+      fvec = fvec:view(fvec:size(1) * settings.inputSize)
+      table.insert(dataset.cache, {inp = fvec, out = references})
+      
+      -- save counts of samples per file to table
+      globalSamples = globalSamples + nSamples
+      nSamples = nSamples - settings.seqL - settings.seqR
+      table.insert(dataset.nSamplesList, nSamples)
+      
+      -- calculate total samples
+      if nSamples >= 1 then
+        totalSamples = totalSamples + nSamples
+      end
+      
     end
     
+    -- save package to files
+    if savePackageData then
+      
+      savePackage(dataset.cache, dataset.nSamplesList, globalSamples, totalSamples, #filelist, listName)      
+      log.info('Package saved in ' .. sys.clock() - begin)
+      return dataset
+    end
+  
+  -- load saved package
+  else
+    local nSamples, sampPeriod, sampSize, parmKind, fvec, references, filesCount
+    nSamples, sampPeriod, sampSize, parmKind, fvec, references, globalSamples, totalSamples, filesCount, dataset.nSamplesList = readPackage(listName)    
+    fvec = fvec:view(fvec:size(1) * settings.inputSize)
+    table.insert(dataset.cache, { inp = fvec, out = references })
   end
 
   -- prepare tensors for data for training
   dataset.index.file = torch.Tensor(totalSamples):int()    -- tensor -> frames vs. file
   dataset.index.pos = torch.Tensor(totalSamples):int()     -- tensor -> frames vs. position in file
-  
+    
   -- fill tensors accordingly to allow training
-  for ll = 1, #dataset.nSamplesList, 1 do   
-    local nSamples = dataset.nSamplesList[ll]
-    if nSamples >= 1 then     -- sanity check
-      i = settings.seqL
-      dataset.index.file:narrow(1, dataset.nSamples + 1, nSamples):fill(ll)               -- fe [1 1 1 2 2 2 2 2]
-      dataset.index.pos:narrow(1, dataset.nSamples + 1, nSamples):apply(function(x)       -- fe [1 2 3 1 2 3 4 5] + seqL
-        i = i + 1
-        return i
-      end)
-      -- compute final number of samples
-      dataset.nSamples = dataset.nSamples + nSamples   
-    end
+  if not loadPackageData then 
+    for ll = 1, #dataset.nSamplesList, 1 do   
+      local nSamples = dataset.nSamplesList[ll]
+      if nSamples >= 1 then     -- sanity check
+        i = settings.seqL
+        dataset.index.file:narrow(1, dataset.nSamples + 1, nSamples):fill(ll)               -- fe [1 1 1 2 2 2 2 2]
+        dataset.index.pos:narrow(1, dataset.nSamples + 1, nSamples):apply(function(x)       -- fe [1 2 3 1 2 3 4 5] + seqL
+          i = i + 1
+          return i
+        end)
+        -- compute final number of samples
+        dataset.nSamples = dataset.nSamples + nSamples   
+      end
+    end  
+  else
+    local i = 0
+    for ll = 1, #dataset.nSamplesList, 1 do   
+      local nSamples = dataset.nSamplesList[ll]
+      if nSamples >= 1 then     -- sanity check
+        i = i + settings.seqL
+        dataset.index.file:narrow(1, dataset.nSamples + 1, nSamples):fill(1)                
+        dataset.index.pos:narrow(1, dataset.nSamples + 1, nSamples):apply(function(x)       -- fe [1 2 3 13 14 15] + seqL
+          i = i + 1
+          return i
+        end)
+        i = i + settings.seqR
+        -- compute final number of samples
+        dataset.nSamples = dataset.nSamples + nSamples   
+      end
+    end  
   end  
   
   -- prepare mean & std tensors
@@ -153,19 +192,15 @@ function Dataset(list, isFilelist, computeFramestats, decode)
   
   -- get frame + surroundings and ref
   setmetatable (dataset, {__index = function (self, i)
-    -- identify file    
-    local fileid = dataset.index.file[i]
-    
-    -- load file data
-    local currentInput = self.cache[fileid].inp
-    local currentOutput = self.cache[fileid].out
+    -- identify file       
+    local fileid = self.index.file[i]
     
     -- find the indices of asked data
-    local startIndex = (dataset.index.pos[i] - settings.seqL - 1) * settings.inputSize + 1
+    local startIndex = (self.index.pos[i] - settings.seqL - 1) * settings.inputSize + 1
     local endIndex = startIndex + (settings.seqR + 1 + settings.seqL) * settings.inputSize - 1
-    
+
     -- clone the asked data   
-    local inp = currentInput[{{startIndex, endIndex}}]:clone()   
+    local inp = self.cache[fileid].inp[{{startIndex, endIndex}}]:clone()   
     
     -- normalize
     inp:add(-mean)
@@ -174,7 +209,7 @@ function Dataset(list, isFilelist, computeFramestats, decode)
     -- prepare the output
     local out
     if not decode then
-      out = (currentOutput[dataset.index.pos[i]] + 1)
+      out = (self.cache[fileid].out[self.index.pos[i]] + 1)
     end
     
     -- return the asked data
