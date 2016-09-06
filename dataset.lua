6344a6e2d2fa9548a98edf0e496448f2b5064134
1shark1 <lukas.mateju1@gmail.com>
2016-09-06 15:31:27 +0200

    faster check of files suitability for training

--- dataset.lua
+++ dataset.lua
@@ -33,7 +33,6 @@ function Dataset(list, isFilelist, computeFramestats, loadPackageData, savePacka
   
   -- initialize sample count
   local totalSamples = 0
-  local globalSamples = 0
   
   -- process data / load saved package
   if not loadPackageData then
@@ -70,111 +69,110 @@ function Dataset(list, isFilelist, computeFramestats, loadPackageData, savePacka
         nSamples, sampPeriod, sampSize, parmKind, fvec = readInputs(filelist[file] .. settings.parExt)
       end
       
-      -- read refs files
-      local references
-      if not decode then 
-        if settings.sameFolder == 0 then
-          filelist[file] = filelist[file]:gsub(settings.parPath, settings.refPath)
-        end
+      -- filter short recordings for training
+      if nSamples - settings.seqL - settings.seqR > 0 or (settings.cloneBorders == 1 and nSamples > 0) then
+      
+        -- read refs files
+        local references
+        if not decode then 
+          if settings.sameFolder == 0 then
+            filelist[file] = filelist[file]:gsub(settings.parPath, settings.refPath)
+          end
 
-        if settings.inputView > 0 then
-          references = viewRefs
-        else
-          references = readRefs(filelist[file] .. settings.refExt, nSamples)
-        end    
-        
-        -- compute framestats
-        if computeFramestats then
-          for i = 1, references:size(1), 1 do
-            if references[i] < settings.outputSize then
-              dataset.framestats[references[i]+1] = dataset.framestats[references[i]+1] + 1
+          if settings.inputView > 0 then
+            references = viewRefs
+          else
+            references = readRefs(filelist[file] .. settings.refExt, nSamples)
+          end    
+          
+          -- compute framestats
+          if computeFramestats then
+            for i = 1, references:size(1), 1 do
+              if references[i] < settings.outputSize then
+                dataset.framestats[references[i]+1] = dataset.framestats[references[i]+1] + 1
+              end
             end
           end
         end
-      end
 
-      -- clone borders
-      if settings.cloneBorders == 1 then
-        fvec = cloneBordersInputs(fvec)
-        if not decode then 
-          references = cloneBordersRefs(references)
+        -- clone borders
+        if settings.cloneBorders == 1 then
+          fvec = cloneBordersInputs(fvec)
+          if not decode then 
+            references = cloneBordersRefs(references)
+          end
+          nSamples = nSamples + settings.seqL + settings.seqR
         end
-        nSamples = nSamples + settings.seqL + settings.seqR
-      end
         
-      -- apply CMS
-      if settings.applyCMS == 1 then
-        applyCMS(fvec, nSamples)
-      end    
+        -- apply CMS
+        if settings.applyCMS == 1 then
+          applyCMS(fvec, nSamples)
+        end    
       
-      -- store data to memory
-      fvec = fvec:view(fvec:size(1) * settings.inputSize)
-      table.insert(dataset.cache, {inp = fvec, out = references})
+        -- store data to memory
+        fvec = fvec:view(fvec:size(1) * settings.inputSize)
+        table.insert(dataset.cache, {inp = fvec, out = references})
       
-      -- save counts of samples per file to table
-      globalSamples = globalSamples + nSamples
-      nSamples = nSamples - settings.seqL - settings.seqR
-      table.insert(dataset.nSamplesList, nSamples)
+        -- save counts of samples per file to table
+        nSamples = nSamples - settings.seqL - settings.seqR
+        table.insert(dataset.nSamplesList, nSamples)
       
-      -- calculate total samples
-      if nSamples >= 1 then
+        -- calculate total samples
         totalSamples = totalSamples + nSamples
-      end
       
+      end
     end
     
     -- save package to files
     if savePackageData then
-      
-      savePackage(dataset.cache, dataset.nSamplesList, globalSamples, totalSamples, #filelist, listName)      
+      savePackage(dataset.cache, dataset.nSamplesList, totalSamples, #dataset.nSamplesList, listName)      
       log.info('Package saved in ' .. sys.clock() - begin)
       return dataset
     end
-  
+    
+    -- prepare tensors for data for training
+    dataset.index.file = torch.Tensor(totalSamples):int()    -- tensor -> frames vs. file
+    dataset.index.pos = torch.Tensor(totalSamples):int()     -- tensor -> frames vs. position in file
+    
+    -- fill tensors accordingly to allow training
+    for ll = 1, #dataset.nSamplesList, 1 do   
+      local i = settings.seqL
+      dataset.index.file:narrow(1, dataset.nSamples + 1, dataset.nSamplesList[ll]):fill(ll)               -- fe [1 1 1 2 2 2 2 2]
+      dataset.index.pos:narrow(1, dataset.nSamples + 1, dataset.nSamplesList[ll]):apply(function(x)       -- fe [1 2 3 1 2 3 4 5] + seqL
+        i = i + 1
+        return i
+      end)
+      -- compute final number of samples
+      dataset.nSamples = dataset.nSamples + dataset.nSamplesList[ll]   
+    end  
+      
   -- load saved package
   else
-    local nSamples, sampPeriod, sampSize, parmKind, fvec, references, filesCount
-    nSamples, sampPeriod, sampSize, parmKind, fvec, references, globalSamples, totalSamples, filesCount, dataset.nSamplesList = readPackage(listName)    
+  
+  -- load package
+    local nSamples, sampPeriod, sampSize, parmKind, fvec, references
+    nSamples, sampPeriod, sampSize, parmKind, fvec, references, totalSamples, dataset.nSamplesList = readPackage(listName)      
     fvec = fvec:view(fvec:size(1) * settings.inputSize)
     table.insert(dataset.cache, { inp = fvec, out = references })
-  end
-
-  -- prepare tensors for data for training
-  dataset.index.file = torch.Tensor(totalSamples):int()    -- tensor -> frames vs. file
-  dataset.index.pos = torch.Tensor(totalSamples):int()     -- tensor -> frames vs. position in file
     
-  -- fill tensors accordingly to allow training
-  if not loadPackageData then 
-    for ll = 1, #dataset.nSamplesList, 1 do   
-      local nSamples = dataset.nSamplesList[ll]
-      if nSamples >= 1 then     -- sanity check
-        i = settings.seqL
-        dataset.index.file:narrow(1, dataset.nSamples + 1, nSamples):fill(ll)               -- fe [1 1 1 2 2 2 2 2]
-        dataset.index.pos:narrow(1, dataset.nSamples + 1, nSamples):apply(function(x)       -- fe [1 2 3 1 2 3 4 5] + seqL
-          i = i + 1
-          return i
-        end)
-        -- compute final number of samples
-        dataset.nSamples = dataset.nSamples + nSamples   
-      end
-    end  
-  else
+    -- prepare tensors for data for training
+    dataset.index.file = torch.Tensor(totalSamples):int()    -- tensor -> frames vs. file
+    dataset.index.pos = torch.Tensor(totalSamples):int()     -- tensor -> frames vs. position in file
+    
+    -- fill tensors accordingly to allow training   
     local i = 0
     for ll = 1, #dataset.nSamplesList, 1 do   
-      local nSamples = dataset.nSamplesList[ll]
-      if nSamples >= 1 then     -- sanity check
-        i = i + settings.seqL
-        dataset.index.file:narrow(1, dataset.nSamples + 1, nSamples):fill(1)                
-        dataset.index.pos:narrow(1, dataset.nSamples + 1, nSamples):apply(function(x)       -- fe [1 2 3 13 14 15] + seqL
-          i = i + 1
-          return i
-        end)
-        i = i + settings.seqR
-        -- compute final number of samples
-        dataset.nSamples = dataset.nSamples + nSamples   
-      end
-    end  
-  end  
+      i = i + settings.seqL
+      dataset.index.file:narrow(1, dataset.nSamples + 1, dataset.nSamplesList[ll]):fill(1)                
+      dataset.index.pos:narrow(1, dataset.nSamples + 1, dataset.nSamplesList[ll]):apply(function(x)       -- fe [1 2 3 13 14 15] + seqL
+        i = i + 1
+        return i
+      end)
+      i = i + settings.seqR
+      -- compute final number of samples
+      dataset.nSamples = dataset.nSamples + dataset.nSamplesList[ll]  
+    end
+  end
   
   -- prepare mean & std tensors
   local mean = settings.mean:repeatTensor(settings.seqL + settings.seqR + 1)
