a0a0ec63b62a71c0b04f0dc4b906497efe3ce1fc
1shark1 <lukas.mateju1@gmail.com>
2016-10-05 17:10:31 +0200

    minor optimization + altered logic of border completion

--- set-utils.lua
+++ set-utils.lua
@@ -1,19 +1,19 @@
 
--- LM -- Stats/Dataset Utils -- 4/6/16 --
+-- LM -- Stats/Dataset Utils -- 5/10/16 --
 
 
 
 -- function applying local mean subtraction/CMS to input data
-function applyCMS(fvec, nSamples) 
+function applyCMS(fvec) 
   
   -- init
-  local cms = torch.Tensor(nSamples, settings.inputSize):zero()
+  local cms = torch.Tensor(fvec:size(1), settings.inputSize):zero()
   local cmsCounter
   
   -- indices for computation
   local startIndex, endIndex
     
-  for i = 1, nSamples, 1 do
+  for i = 1, fvec:size(1), 1 do
     -- find indices
     cmsCounter = settings.cmsSize + 1
     startIndex = i - (settings.cmsSize / 2)
@@ -26,9 +26,9 @@ function applyCMS(fvec, nSamples)
     end
     
     -- ending exception
-    if endIndex > nSamples then
-      cmsCounter = cmsCounter - (endIndex - nSamples)
-      endIndex = nSamples
+    if endIndex > fvec:size(1) then
+      cmsCounter = cmsCounter - (endIndex - fvec:size(1))
+      endIndex = fvec:size(1)
     end
     
     -- compute CMS for given frame   
@@ -42,34 +42,120 @@ end
 
 
 
--- function cloning borders - inputs
-function cloneBordersInputs(fvec) 
+-- function filling borders of input
+function fillBordersInputs(fvec)
   
-  local pre = fvec[{{1, {}}}]
-  local post = fvec[{{-1, {}}}]
-
-  pre = pre:repeatTensor(settings.seqL, 1)
-  post = post:repeatTensor(settings.seqR, 1)  
-  
-  return torch.cat(pre, torch.cat(fvec, post, 1), 1)
+  -- switch to correct fill of boarders (inputs)
+  if settings.cloneBorders == 1 then
+    return cloneBordersInputs(fvec)
+  elseif settings.cloneBorders == 2 then
+    return fillValueBordersInputs(fvec, 0)
+  else
+    error('FillBordersInputType: not supported')
+  end
   
 end
 
 
 
--- function cloning borders - targets
-function cloneBordersRefs(data) 
+-- function cloning borders - inputs
+function cloneBordersInputs(fvec) 
   
-  local curOut = torch.Tensor(data:size(1) + settings.seqL + settings.seqR)  
+  local pre = fvec[{{1, {}}}]
+  local post = fvec[{{-1, {}}}]
+  
+  -- clone borders
+  if settings.seqL > 0 and settings.seqR > 0 then
+    pre = pre:repeatTensor(settings.seqL, 1)
+    post = post:repeatTensor(settings.seqR, 1)  
+    return torch.cat(pre, torch.cat(fvec, post, 1), 1)
+  elseif settings.seqL == 0 and settings.seqR > 0 then
+    post = post:repeatTensor(settings.seqR, 1)  
+    return torch.cat(fvec, post, 1)
+  elseif settings.seqL > 0 and settings.seqR == 0 then
+    pre = pre:repeatTensor(settings.seqL, 1)
+    return torch.cat(pre, fvec, 1)
+  end
+  
+  return fvec
+end
+
+
+
+-- function filling borders with number value - inputs
+function fillValueBordersInputs(fvec, value) 
+  
+  local pre, post
+  
+  -- fill borders
+  if settings.seqL > 0 and settings.seqR > 0 then
+    pre = torch.Tensor(settings.seqL, settings.inputSize):zero() + value
+    post = torch.Tensor(settings.seqR, settings.inputSize):zero() + value
+    return torch.cat(pre, torch.cat(fvec, post, 1), 1)
+  elseif settings.seqL == 0 and settings.seqR > 0 then
+    post = torch.Tensor(settings.seqR, settings.inputSize):zero() + value 
+    return torch.cat(fvec, post, 1)
+  elseif settings.seqL > 0 and settings.seqR == 0 then
+    pre = torch.Tensor(settings.seqL, settings.inputSize):zero() + value
+    return torch.cat(pre, fvec, 1)
+  end
+  
+  return fvec
+
+end
+
+
+
+-- function filling borders of references
+function fillBordersRefs(refs)
+  
+  -- switch to correct fill of boarders (references)
+  if settings.cloneBorders == 1 then
+    return cloneBordersRefs(refs)
+  elseif settings.cloneBorders == 2 then
+    return fillValueBordersRefs(refs, -1)
+  else
+    error('FillBordersInputType: not supported')
+  end
+  
+end
+
+
+
+-- function cloning borders - references
+function cloneBordersRefs(refs) 
+  
+  local curOut = torch.Tensor(refs:size(1) + settings.seqL + settings.seqR)  
   
   for i = 1, settings.seqL, 1 do
-    curOut[i] = data[1]
+    curOut[i] = refs[1]
   end  
   for i = settings.seqL + 1, curOut:size(1) - settings.seqR - settings.seqL, 1 do
-    curOut[i] = data[i]
+    curOut[i] = refs[i]
   end  
   for i = curOut:size(1) - settings.seqR - settings.seqL + 1, curOut:size(1) - settings.seqL, 1 do
-    curOut[i] = data[data:size(1)]
+    curOut[i] = refs[refs:size(1)]
+  end    
+
+  return curOut
+  
+end
+
+
+
+-- function cloning borders - references
+function fillValueBordersRefs(refs, val) 
+  
+  local curOut = torch.Tensor(refs:size(1) + settings.seqL + settings.seqR)  
+  
+  for i = 1, settings.seqL, 1 do
+    curOut[i] = val
+  end  
+  for i = settings.seqL + 1, curOut:size(1) - settings.seqR - settings.seqL, 1 do
+    curOut[i] = refs[i]
+  end  
+  for i = curOut:size(1) - settings.seqR - settings.seqL + 1, curOut:size(1) - settings.seqL, 1 do
+    curOut[i] = val
   end    
 
   return curOut
